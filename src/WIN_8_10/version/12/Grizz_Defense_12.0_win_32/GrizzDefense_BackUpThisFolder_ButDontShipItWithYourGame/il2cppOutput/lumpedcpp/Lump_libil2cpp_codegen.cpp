a2f5fce0a7da251d1340e850a97a4ec2d3bfce7b
Shane <troyanshane@gmail.com>
2021-03-29 21:07:47 -0400

    Delete src/WIN_8_10/version/12/Grizz_Defense_12.0_win_32 directory

--- src/WIN_8_10/version/12/Grizz_Defense_12.0_win_32/GrizzDefense_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/lumpedcpp/Lump_libil2cpp_codegen.cpp
+++
@@ -1,3 +0,0 @@
-#include "il2cpp-config.h"
-#include "C:\Program Files\Unity Editor\2019.2.9f1\Editor\Data\il2cpp\libil2cpp\codegen\il2cpp-codegen-common.cpp"
-#include "C:\Program Files\Unity Editor\2019.2.9f1\Editor\Data\il2cpp\libil2cpp\codegen\il2cpp-codegen-il2cpp.cpp"
--- src/WIN_8_10/version/12/Grizz_Defense_12.0_win_32/GrizzDefense_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/lumpedcpp/Lump_libil2cpp_mono-runtime.cpp
+++
@@ -1,3 +0,0 @@
-#include "il2cpp-config.h"
-#include "C:\Program Files\Unity Editor\2019.2.9f1\Editor\Data\il2cpp\libil2cpp\mono-runtime\il2cpp-callbacks.cpp"
-#include "C:\Program Files\Unity Editor\2019.2.9f1\Editor\Data\il2cpp\libil2cpp\mono-runtime\il2cpp-mono-support.cpp"
